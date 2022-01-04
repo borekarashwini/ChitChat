91fcba59737ba470c2223f8b09feded24f6081e5
Ashwini <borekarashwini24@gmail.com>
2022-01-04 20:53:56 +0530

    Final commit

--- app/src/main/java/com/example/chitchat/DashboardActivity.java
+++ app/src/main/java/com/example/chitchat/DashboardActivity.java
@@ -3,9 +3,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
